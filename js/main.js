5e8456d70c8cd004bb951f455707a66ed4a6dab0
Whatisloooove <60188730+Whatisloooove@users.noreply.github.com>
2020-05-03 19:06:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+
+$('.studia__row').slick({
+    dots: false,
+    infinite: true,
+    speed: 1800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+$(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
+    });
+});
